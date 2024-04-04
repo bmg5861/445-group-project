719cea57ce94cfb9d512086978d4f71568eab6d2
bmg5861 <bmg5861@psu.edu>
2024-04-04 14:47:16 -0400

    setting up classification model with dummy data

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
-import tensorflow as ts
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
 print("TensorFlow version:", tf.__version__)
+
+# testing model on clothing data set 
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Data preprocessing
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+# Scaling values to 0 or 1, divied preprocess images against 255 due to the amount of pixels in the image 
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+# Verify & Format data to 25 iamges
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+## Building the model
+
+# Seting up layers 
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+# Compiling the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Training the model
+model.fit(train_images, train_labels, epochs=10)
+
+
+# Evaluating results
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
 
